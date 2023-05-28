311cca4bca9aebc491a4956cd923506e8816641c
samwiseGhandi <samellow212@gmail.com>
2023-05-29 00:27:30 +0200

    latest checkpoint

--- pages/sequencer.vue
+++ pages/sequencer.vue
@@ -13,7 +13,7 @@
         <!-- Sequencer -->
         <div class="sequencer">
           <!-- Controls -->
-          <div class="controls flex mb-4 w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow">
+          <div class="controls flex mb-4 w-full p-4 text-center bg-gray-200 border border-gray-200 rounded-lg shadow">
             <button @click="play" class="mr-2 bg-gray-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
               :disabled="isPlaying">Play</button>
             <button @click="pause" class="mr-2 bg-gray-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
@@ -27,41 +27,26 @@
               <input type="range" class="tempo-slider flex-1" min="40" max="300" v-model="tempo" />
             </div>
           </div>
+
           <!-- Pattern area -->
-          <div class="grid-patterns mb-4 w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow">
-
-            <h2 class="track">track-1</h2>
-            <button v-for="step in 16" :class="`step track-1-${step - 1}`" :key="step">
-              <span class="mobile-num">{{ step }}</span>
-            </button>
-
-            <h2 class="track">track-2</h2>
-            <button v-for="step in 16" :class="`step track-1-${step - 1}`" :key="step">
-              <span class="mobile-num">{{ step }}</span>
-            </button>
-
-            <h2 class="track">track-3</h2>
-            <button v-for="step in 16" :class="`step track-1-${step - 1}`" :key="step">
-              <span class="mobile-num">{{ step }}</span>
-            </button>
-
-            <h2 class="track">track-4</h2>
-            <button v-for="step in 16" :class="`step track-1-${step - 1}`" :key="step">
-              <span class="mobile-num">{{ step }}</span>
-            </button>
-
-            <div class="step-num"></div>
-            <div class="step-num bar-start" v-for="step in 16" :key="step">{{ step }}</div>
+          <div class="mb-4 w-full p-4 text-center bg-gray-200 border border-gray-200 rounded-lg shadow">
+            <div class="grid-column patterns flex mb-1" v-for="track in amountOfTracks">
+              <h2 class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded track-{{ track }}">
+                Track-{{ track }}</h2>
+              <button v-for="step in 16" class="w-10 h-10" :key="(track - 1) * 16 + step"
+                :class="`bg-gray-400 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded track-box-${track}-${step - 1} `"
+                @click="toggleStep(index)">
+                <span class="step-number">{{ step }}</span>
+              </button>
+            </div>
+            <div class="grid-column">
+              <h2 class="p-1 track"></h2>
+              <div v-for="step in 16" class="w-10 h-4" :key="(step - 1) * 16 + step"
+                :class="`p-1 track-box-${track}-${step - 1}`">
+                <span class="step-nr text-xs">{{ step }}</span>
+              </div>
+            </div>
           </div>
-
-          <!-- <div class="grid-pattern patterns mb-4 w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow"
-            v-for="track in 4">
-            <h2 class="bg-gray-400 p-1 track-{{ track }}">Track-{{ track }}</h2>
-            <button v-for="step in 16" class="w-10 h-10" :key="(track - 1) * 16 + step"
-              :class="`bg-gray-200 step p-1 track-box-${track}-${step - 1}`">
-              <span class="step-number">{{ step }}</span>
-            </button>
-          </div> -->
 
           <!-- <audio class="track-1-sound" src=""></audio>
           <audio class="track-2-sound" src=""></audio>
@@ -77,9 +62,39 @@
 export default {
   data() {
     return {
-      tempo: 120
+      sequence: [],
+      isPlaying: false,
+      currentStep: 0,
+      tempo: 120,
+      amountOfTracks: 4,
     };
-  }
+  },
+  methods: {
+    toggleStep(index) {
+      this.sequence[index] = !this.sequence[index];
+    },
+    play() {
+      this.isPlaying = true;
+      this.playStep();
+    },
+    playStep() {
+      if (this.sequence[this.currentStep]) {
+        console.log(`Step ${this.currentStep} triggered.`);
+        //logic here for playback
+      }
+      this.currentStep = (this.currentStep + 1) % this.sequence.length;
+      if (this.isPlaying) {
+        setTimeout(this.playStep, 300); // Adjust the timing interval as needed
+      }
+    },
+    pause() {
+      this.isPlaying = false;
+    },
+    stop() {
+      this.isPlaying = false;
+      this.currentStep = 0;
+    },
+  },
 };
 </script>
 
@@ -87,9 +102,31 @@ export default {
 /* Add your CSS styles here */
 .grid-patterns {
   display: grid;
-  grid-template-columns: 100px repeat(16, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-  gap: 1rem;
+  grid-template-columns: 120px repeat(16, 1fr);
+  /* 16 columns */
+  grid-template-rows: repeat(4, auto);
+  /* 4 rows */
+  grid-gap: 4px;
+  /* Adjust the gap between cells as needed */
+}
+
+h2 {
+  width: 120px;
+}
+
+.grid-row {
+  display: grid;
+  /* grid-template-columns: 120 repeat(16, 1fr); */
+  /* First column auto, rest 15 columns */
+  align-items: center;
+  gap: 4px;
+  /* Adjust the gap between cells as needed */
+}
+
+.grid-column {
+  display: flex;
+  justify-content: center;
+  gap: 4px;
 }
 
 .tempo-slider {
@@ -101,7 +138,10 @@ export default {
 }
 
 .step {
-  background: rgba(255, 255, 255, 0.15);
   cursor: pointer;
+}
+
+.step-number {
+  display: none;
 }
 </style>
